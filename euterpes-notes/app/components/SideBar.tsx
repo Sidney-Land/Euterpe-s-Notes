a1672318cb4004150d52945d18ddce886c2203a2
Tyler <ruescht23@students.ecu.edu>
2026-03-13 15:46:50 -0400

    added comments that discuss the changes made.

--- euterpes-notes/app/components/SideBar.tsx
+++ euterpes-notes/app/components/SideBar.tsx
@@ -6,8 +6,13 @@ const SideBar = (props: SideBarProp) => {
     const SideBarStyle: CSSProperties = {
       //keeps the sidebar in place if the user scrolls the page
       position: 'fixed',
-      //marginTop: '32px',
+      
+      //vh is the same as % except it looks at the space on screen, while % looks at the given 
+      //height of the parent component.
+
+      //top tells the component where to start
       top: '10vh',
+      //calculates the height by subtracting the height of the header
       height: 'calc(100vh - 10vh)',
       width: '240px',
     }
