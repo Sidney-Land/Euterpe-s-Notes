d015a5bbfb0b320cda333ae086eab3a24f275af7
Sidney Land <lands24@students.ecu.edu>
2026-03-15 18:02:57 -0400

    Re-Added Sidebar Buttons, new Global Button CSS

--- euterpes-notes/app/components/SideBar.tsx
+++ euterpes-notes/app/components/SideBar.tsx
@@ -15,10 +15,26 @@ const SideBar = (props: SideBarProp) => {
       //calculates the height by subtracting the height of the header
       height: 'calc(100vh - 10vh)',
       width: '240px',
+
+      display: 'inline-flex',
+      alignItems: 'anchor-center', //aligns the buttons to the center of the sidebar
+      flexDirection: 'column',
+      justifyContent: 'flex-start',
+      background: '#292929',
+      border: '2px solid #e2e8f0',
+      color: '#ffffff', //text color
     }
+
+    // TODO: Add logic to route to user's profile if signed in, or to the authentication page if not
+    function ProfileLink():string {
+        return "";
+    }
+
+    // TODO: Add link to Recent Posts (useful for going to Recent Posts Feed if signed in)
     return (
     <div className = "component-style" style = {SideBarStyle} >
-        Placeholder SideBar
+      <a className = "button" href= {ProfileLink()}>Profile</a>
+      <a className = "button" href= ''>Recent Posts</a>
     </div>
   );
 }
