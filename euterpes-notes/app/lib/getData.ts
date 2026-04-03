90bed4c98d7c71ce2c72f2325c34d1ff383bfe94
Tyler <ruescht23@students.ecu.edu>
2026-04-03 17:03:36 -0400

    updated getData.ts so that it should communicate with supabase

--- euterpes-notes/app/lib/getData.ts
+++ euterpes-notes/app/lib/getData.ts
@@ -1,9 +1,22 @@
 'use server'
 import 'server-only'
-import {supabase} from './supabaseClient'
+import { supabase } from './supabaseClient'; // Use the client you already made!
 
 export async function getPost(post_id: string) {
-    const response = await fetch(`https://api.vercel.app/blog/${post_id}`);
-    const data = await response.json();
+    // We tell Supabase: "Give me the post where the id matches our postId"
+    const { data, error } = await supabase
+        .from("Post") 
+        .select('*')
+        .eq('id', post_id)
+        .single(); // Since we only want one post
+
+    if (error) {
+        console.error("Error fetching post:", error);
+        return null;
+    }
+
     return data;
 }
+
+//calls get requests for each individual post. should swap this to a batch fetch method later.
+//pagination would give us the page system rather than an infinite scroll.
